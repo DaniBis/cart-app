c4d76a976cc5255933caf29bb84dec7eab1c323b
DaniBis <bisceanudaniel@gmail.com>
2023-05-22 18:40:44 +0700

    create the add button and the functionality

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,37 +1,99 @@
-import React, { useState, useEffect } from 'react'
-import { db } from './../../config/config';
-import { getDocs, collection } from 'firebase/firestore';
-import { ItemsList } from './shopStyle';
+import React, { useState, useEffect } from 'react';
+import { db, storage } from './../../config/config';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { getDocs, collection, addDoc } from 'firebase/firestore';
+import { ItemsList, AddForm, PerItem } from './shopStyle';
+import { v4 } from 'uuid';
 
 export const Shop = () => {
   const [items, setItems] = useState([]);
-  const itemsRef = collection(db,"items");
+  const itemsRef = collection(db, "items");
 
-  useEffect(()=>{
+  // New item states
+  const [newItemName, setNewItemName] = useState("");
+  const [newItemPrice, setNewItemPrice] = useState(0);
+  const [newItemImage, setNewItemImage] = useState("");
+
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getDocs(itemsRef);
-        const filterData = data.docs.map((doc) => ({
+        const filteredData = data.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
         }));
-        setItems(filterData);
-      } catch (error) {
-        console.error(error);
+        setItems(filteredData);
+      } catch (err) {
+        console.error(err);
       }
     };
     fetchData();
   }, []);
 
+  function handleImage(e) {
+    e.preventDefault();
+    let pickedFile;
+    if (e.target.files && e.target.files.length > 0) {
+      pickedFile = e.target.files[0];
+      setNewItemImage(pickedFile);
+      console.log(pickedFile);
+    }
+  }
+
+  const addItems = async (e) => {
+  e.preventDefault(); 
+  if (newItemImage == null) return;
+  const imageRef = ref(storage, `items/${newItemImage.name + v4()}`);
+  try {
+    await uploadBytes(imageRef, newItemImage);
+    alert("Image uploaded");
+    const downloadURL = await getDownloadURL(imageRef);
+    const newItem = {
+      Image: downloadURL,
+      Name: newItemName,
+      Price: newItemPrice,
+    };
+    await addDoc(itemsRef, newItem);
+    setNewItemImage("");
+    setNewItemName("");
+    setNewItemPrice(0);
+  } catch (err) {
+    console.log(err);
+  }
+}; 
+
   return (
-    <ItemsList>
-      {items.map((item) => (
-        <div key={item.id}>
-          <img src={item.Image} alt={item.Name} />
-          <h1>{item.Name}</h1>
-          <h4>{item.Price}</h4>
-        </div>
-      ))} 
-    </ItemsList>
+    <div>
+      <AddForm onSubmit={addItems}>
+        <input
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+          onChange={handleImage}
+        />
+        <input
+          type="text"
+          placeholder="name"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="price"
+          value={newItemPrice}
+          onChange={(e) => setNewItemPrice(e.target.value)}
+        />
+        <button onClick={addItems}>Add</button>
+      </AddForm>
+      <ItemsList>
+        {items.map((item) => (
+          <PerItem key={item.id}>
+            <img src={item.Image} alt={item.Name} />
+            <h1>{item.Name}</h1>
+            <h4>{item.Price}</h4>
+          </PerItem>
+        ))}
+      </ItemsList>
+    </div>
   );
 };
--- src/pages/shop/shopStyle.jsx
+++ src/pages/shop/shopStyle.jsx
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
-export const ItemsList = styled.div`
-    width: 300px;
+export const PerItem = styled.div`
+    width: 400px;
     & img {
         width: 100%;
     }
     & h1, h4 {
         color: black;
     }
+`;
+
+export const AddForm = styled.div`
+    display: inline-flex;
+`;
+
+export const ItemsList = styled.div`
+    display: inline-flex;
+    width: 100%;
 `;
